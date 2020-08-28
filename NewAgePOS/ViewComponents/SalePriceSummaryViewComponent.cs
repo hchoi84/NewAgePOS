3856f7c9588a41da05a4d4a8f537d1097743f693
Howard Choi <hchoi84@gmail.com>
2020-08-28 10:45:24 -0700

    Aded ViewComponent for Sale Price Summary section

---
+++ NewAgePOS/ViewComponents/SalePriceSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewAgePOSModels.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewAgePOS.ViewComponents
+{
+  public class SalePriceSummaryViewComponent : ViewComponent
+  {
+    public IViewComponentResult Invoke(List<SaleLineModel> saleLines, float taxPct)
+    {
+      SalePriceSummaryModel model = new SalePriceSummaryModel();
+      model.Subtotal = saleLines.Sum(sl => sl.LineTotal);
+      model.Discount = saleLines.Sum(sl => (sl.Qty * sl.DiscAmt) + (sl.Qty * (sl.Price * (sl.DiscPct / 100f))));
+      model.TaxPct = taxPct;
+      model.Tax = (model.Subtotal - model.Discount) * (taxPct / 100f);
+      model.Total = model.Subtotal - model.Discount + model.Tax;
+      
+      return View(model);
+    }
+  }
+}
---
+++ NewAgePOSModels/Models/SalePriceSummaryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace NewAgePOSModels.Models
+{
+  public class SalePriceSummaryModel
+  {
+    public float Subtotal { get; set; }
+    public float Discount { get; set; }
+    public float TaxPct { get; set; }
+    public float Tax { get; set; }
+    public float Total { get; set; }
+    public CultureInfo Dollar { get; } = new CultureInfo("en-US");
+  }
+}
