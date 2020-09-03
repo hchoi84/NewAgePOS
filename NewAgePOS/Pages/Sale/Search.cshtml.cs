73570433078042b03b2e0e1278ec51a03b40140c
Howard Choi <hchoi84@gmail.com>
2020-09-02 21:13:45 -0700

    Changed Sale Search procedure

--- NewAgePOS/Pages/Sale/Search.cshtml.cs
+++ NewAgePOS/Pages/Sale/Search.cshtml.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NewAgePOSLibrary.Data;
 using NewAgePOSModels.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 
 namespace NewAgePOS.Pages.Sale
@@ -18,31 +19,61 @@ namespace NewAgePOS.Pages.Sale
     }
 
     [BindProperty(SupportsGet = true)]
-    [Display(Name = "Sale Id")]
-    [Range(0, int.MaxValue)]
     public int SaleId { get; set; }
 
     [BindProperty(SupportsGet = true)]
-    [Display(Name = "Last Name")]
-    [MinLength(2, ErrorMessage = "Min {1} characters")]
-    public string LastName { get; set; }
+    public string SearchMethod { get; set; }
 
     [BindProperty(SupportsGet = true)]
-    [Display(Name = "Email Address")]
-    [DataType(DataType.EmailAddress)]
-    public string EmailAddress { get; set; }
+    public string SearchQuery { get; set; }
 
-    [BindProperty(SupportsGet = true)]
-    [Display(Name = "Phone Number")]
-    [StringLength(10, MinimumLength = 10)]
-    public string PhoneNumber { get; set; }
-
-    [BindProperty(SupportsGet = true)]
+    [BindProperty]
     public List<SaleSearchResultModel> Results { get; set; }
+
+    public List<SelectListItem> SearchMethods { get; } = new List<SelectListItem>
+    {
+      new SelectListItem { Text = "Sale Id", Value = "SaleId" },
+      new SelectListItem { Text = "Last Name", Value = "LastName" },
+      new SelectListItem { Text = "Email Address", Value = "EmailAddress" },
+      new SelectListItem { Text = "Phone Number", Value = "PhoneNumber" },
+    };
 
     public IActionResult OnGet()
     {
-      Results = _sqlDb.SearchSales(SaleId, LastName, EmailAddress, PhoneNumber);
+      if (string.IsNullOrEmpty(SearchMethod)) return Page();
+
+      if (SearchMethod == "SaleId")
+      {
+        bool isValid = Int32.TryParse(SearchQuery, out int saleId);
+
+        if (!isValid && saleId > 0)
+        {
+          TempData["Message"] = "Invalid SaleId";
+          return Page();
+        }
+
+        Results = _sqlDb.SearchSales(saleId, "", "", "");
+      }
+      else if (SearchMethod == "LastName")
+      {
+        Results = _sqlDb.SearchSales(0, SearchQuery, "", "");
+      }
+      else if (SearchMethod == "EmailAddress")
+      {
+        if (!SearchQuery.Contains("@"))
+        {
+          TempData["Message"] = "Not a valid email address";
+          return Page();
+        }
+
+        Results = _sqlDb.SearchSales(0, "", SearchQuery, "");
+      }
+      else
+      {
+        bool isValid = Int64.TryParse(SearchQuery, out long phoneNumber);
+
+        if (isValid && SearchQuery.Length == 10) Results = _sqlDb.SearchSales(0, "", "", SearchQuery);
+      }
       
       if (Results != null)
       {
@@ -64,7 +95,7 @@ namespace NewAgePOS.Pages.Sale
     {
       _sqlDb.Sales_CancelById(SaleId);
       TempData["Message"] = "Sale has been cancelled";
-      return RedirectToPage(new { SaleId, LastName, EmailAddress, PhoneNumber });
+      return RedirectToPage(new { SearchMethod, SearchQuery });
     }
   }
 }
--- NewAgePOS/wwwroot/js/site.js
+++ NewAgePOS/wwwroot/js/site.js
@@ -14,3 +14,23 @@
     xferBtn.className = 'btn btn-primary';
   }
 }
+
+function searchQueryMsg(id) {
+  var selElem = document.getElementById('SearchMethod');
+  var optVal = selElem.options[selElem.selectedIndex].value;
+
+  var msgElem = document.getElementById(id);
+
+  if (optVal == 'EmailAddress') {
+    msgElem.innerText = 'ex: email@domain.com';
+  }
+  else if (optVal == 'PhoneNumber') {
+    msgElem.innerText = '(ex: 1234567890) 10 digit with no dashes';
+  }
+  else if (optVal == 'SaleId') {
+    msgElem.innerText = 'Numbers only';
+  }
+  else if (optVal == 'LastName') {
+    msgElem.innerText = 'Last name only. Please do not use full name';
+  }
+}
