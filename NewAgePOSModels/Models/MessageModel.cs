d27e00f48e02a05c1345ba90c4d1129d50dd8536
Howard Choi <hchoi84@gmail.com>
2020-09-15 11:09:02 -0700

    - Removed unused models after removing RefundReceipt page - Added MessageModel - Changed TransactionsEnums to TransactionEnum for consistency with   other Model naming convention

---
+++ NewAgePOSModels/Models/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewAgePOSModels.Models
+{
+  public class MessageModel
+  {
+    public int Id { get; set; }
+    public int SaleId { get; set; }
+    public string Message { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+  }
+}
