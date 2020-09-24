448a065b80ef3b3af87decc87ac76e0e828c616f
Howard Choi <hchoi84@gmail.com>
2020-09-23 19:42:13 -0700

    initial timezone conversion test from UTC to PST

--- NewAgePOSModels/Models/SaleModel.cs
+++ NewAgePOSModels/Models/SaleModel.cs
@@ -4,11 +4,20 @@ namespace NewAgePOSModels.Models
 {
   public class SaleModel
   {
+    private DateTime _created;
+
     public int Id { get; set; }
     public int CustomerId { get; set; }
     public int TaxId { get; set; }
     public bool IsComplete { get; set; }
-    public DateTime Created { get; set; }
+    public DateTime Created {
+      get {
+        TimeZoneInfo pst = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+        return TimeZoneInfo.ConvertTimeFromUtc(_created, pst);
+      }
+      set { _created = value; }
+    }
+
     public DateTime Updated { get; set; }
   }
 }
